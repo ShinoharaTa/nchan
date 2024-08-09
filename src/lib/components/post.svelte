4e74946a3efec384d67a2d400198b3f69546e97d
tshinohara <takuma1201@gmail.com>
2024-08-09 19:54:22 +0900

    リプライの表示機能追加

--- src/lib/components/post.svelte
+++ src/lib/components/post.svelte
@@ -5,14 +5,22 @@
 
   export let event: Nostr.Event<Nostr.Kind.Text>;
   const parsed = parseContent(event.content);
+  const reply_tag = event.tags.find(
+    (tag) => tag.includes("e") && tag.includes("reply"),
+  );
+  const reply = reply_tag ? reply_tag[1].slice(0, 10) : null;
 </script>
 
 <div class="outline mb-3">
   <div class="ps-2" style="border-left: 6px solid #{event.pubkey.slice(0, 6)};">
-    <div class="detail pt-1">
-      <Author hex={event.pubkey} style="simple" />
+    <div class="detail pt-1" id="{reply}">
+      <strong>名無しの{event.pubkey.slice(0, 6)}さん</strong>
       {parseCreated(event.created_at)}
+      ID:{event.id.slice(0, 6)}
     </div>
+    {#if reply}
+      <div><a href="#{reply}">{`>>${reply}`}</a></div>
+    {/if}
     <div class="content lf text-break">{parsed.text_without_urls}</div>
     <div class="content lf text-break">{parsed.other_urls}</div>
     {#each parsed.image_urls as image}
