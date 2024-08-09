360db05be508ad1e96347b1e6f281f4aeb1ced62
an-apco <me@apco.dev>
2024-08-10 01:48:22 +0900

    Cleaned header navigation

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,28 +1,5 @@
-<script lang="ts">
-  export let addHeight = 0
-</script>
-
-<div class="container bg-light fixed-top py-2">
-  <section class="d-flex align-items-center">
-    <div class="flex-shrink-0">
-      <slot name="left" />
-    </div>
-    <div class="fs-2 flex-fill px-2 ellipsis text-center">
-      <img src="/logo.svg" alt="" height="32px" />
-    </div>
-    <div class="flex-shrink-0">
-      <slot name="right" />
-    </div>
-  </section>
-  <section>
-    <slot />
-  </section>
-</div>
-<div class="header-padding"></div>
-<div style="height: {addHeight}px"></div>
-
-<style>
-  .header-padding {
-    height: 64px;
-  }
-</style>
+<header class="flex flex-between">
+  <slot name="left" />
+  <img src="/logo.svg" alt="んちゃんねる" height="32px" />
+  <slot name="right" />
+</header>
